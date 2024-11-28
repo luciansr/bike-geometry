adc285e417dc7b13f018910f5d49603c22487641
Lucian Sturião <luciansr@gmail.com>
2024-11-28 00:18:07 +0000

    create menu

--- bike-fit-simulator/src/app/app.component.ts
+++ bike-fit-simulator/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, FooterComponent],
   template: `
+  <div class="container">
     <app-menu></app-menu>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
