8253b899f0b637ab19775ad09b13ccf63715e925
Lucian Sturião <luciansr@gmail.com>
2024-11-28 23:05:38 +0000

    update values using forms and data service

--- bike-fit-simulator/src/app/biker-sizing/biker-sizing.component.ts
+++ bike-fit-simulator/src/app/biker-sizing/biker-sizing.component.ts
@@ -1,28 +1,34 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { BikerSizingDataService } from '../services/sizing/biker-sizing-data.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BikerSizing } from '../services/sizing/biker-sizing';
 
 @Component({
   selector: 'app-biker-sizing',
-  imports: [BrowserModule, FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './biker-sizing.component.html',
-  styleUrl: './biker-sizing.component.css'
+  styleUrl: './biker-sizing.component.css',
 })
 export class BikerSizingComponent {
-  torsoSize: number = 65;
-  lowerLegSize: number = 50;
-  upperLegSize: number = 50;
-  footSize: number = 42;
-  constructor(private bikerSizingDataService: BikerSizingDataService) {}
+  bikerForm: FormGroup;
 
-  updateData() {
-    const data = {
-      torso: this.torsoSize,
-      lowerLeg: this.lowerLegSize,
-      upperLeg: this.upperLegSize,
-      footSize: this.footSize
+  constructor(
+    private bikerSizingDataService: BikerSizingDataService,
+    private formBuilder: FormBuilder
+  ) {
+    const initialValues = {
+      torso: 65,
+      upperLeg: 55,
+      lowerLeg: 65,
+      footSize: 42,
     };
-    this.bikerSizingDataService.updateBikerSizing(data);
+    this.bikerForm = this.formBuilder.group<BikerSizing>(initialValues);
+    this.bikerSizingDataService.updateBikerSizing(initialValues);
+  }
+
+  ngOnInit(): void {
+    this.bikerForm.valueChanges.subscribe((values: BikerSizing) => {
+      this.bikerSizingDataService.updateBikerSizing(values);
+    });
   }
 }
