126ad21f0a530230316f6b4dc42c6015e77824fa
Lucian Sturião <luciansr@gmail.com>
2024-11-30 19:11:54 +0000

    sending debug json to visualization

--- bike-fit-simulator/src/app/visualization/visualization-panel/visualization-panel.component.ts
+++ bike-fit-simulator/src/app/visualization/visualization-panel/visualization-panel.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { BikerVisualizationComponent } from '../biker/biker-visualization/biker-visualization.component';
+import { BikerSizing } from '../../services/sizing/biker-sizing';
+import { BikerSizingDataService } from '../../services/sizing/biker-sizing-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-visualization-panel',
-  imports: [BikerVisualizationComponent],
+  imports: [BikerVisualizationComponent, CommonModule],
   templateUrl: './visualization-panel.component.html',
   styleUrl: './visualization-panel.component.css'
 })
 export class VisualizationPanelComponent {
+  bikerSizingData: BikerSizing;
 
+  constructor(private bikerSizingDataService: BikerSizingDataService) {
+    this.bikerSizingData = bikerSizingDataService.getInitialValue();
+  }
+
+  ngOnInit() {
+    this.bikerSizingDataService.currentData.subscribe((data) => {
+      this.bikerSizingData = data;
+    });
+  }
 }
