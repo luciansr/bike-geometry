1cd1437f4279f83db115d3387665829589de6ce6
Lucian Sturião <luciansr@gmail.com>
2024-11-30 23:01:46 +0000

    change template from app

--- bike-fit-simulator/src/app/components/app.component.ts
+++ bike-fit-simulator/src/app/components/app.component.ts
@@ -3,18 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './base/menu/menu.component';
 import { FooterComponent } from './base/footer/footer.component';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MenuComponent, FooterComponent],
-  template: `
-  <div class="container">
-    <app-menu></app-menu>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
-    </div>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'bike-fit-simulator';
