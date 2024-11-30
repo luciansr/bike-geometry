0ca828f1b8cc878cfd109028d9729cf236c72bfb
Lucian Sturião <luciansr@gmail.com>
2024-11-30 20:01:24 +0000

    binding to svg

--- bike-fit-simulator/src/app/visualization/biker/biker-visualization/biker-visualization.component.ts
+++ bike-fit-simulator/src/app/visualization/biker/biker-visualization/biker-visualization.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { BikerSizingDataService } from '../../../services/sizing/biker-sizing-data.service';
 import { BikerSizing } from '../../../services/sizing/biker-sizing';
 import { CommonModule, NgIf } from '@angular/common';
-import { BikerLegComponent } from '../biker-leg/biker-leg.component';
-import { BikerFootComponent } from '../biker-foot/biker-foot.component';
 
 @Component({
   selector: 'app-biker-visualization',
-  imports: [NgIf, CommonModule, BikerLegComponent, BikerFootComponent],
+  imports: [CommonModule],
   templateUrl: './biker-visualization.component.html',
   styleUrl: './biker-visualization.component.css',
 })
